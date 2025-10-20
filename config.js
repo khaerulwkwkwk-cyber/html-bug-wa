f5e6bf7cc7d6dfb7f5388efb7e936a43a501edd4
khaerulwkwkwk-cyber <khaerulwkwkwk@gmail.com>
2025-10-20 17:55:53 +0700

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-//credit By Limzzzhama 
-//no hapus credit ketauan hapus hitam//
-
-module.exports = {
-  domain: "https://kalzztamvan-priv-notsepuh.dzhost.my.id", // Ubah jadi Domain panel Mu !!!
-  port: "19027" // Ubah Jadi Port Panel Mu !!!
-};
